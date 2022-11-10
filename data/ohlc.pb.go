24a8cd2bb65e94adb95950ba8112df339096c757
Woody1193 <ryanwood1193@gmail.com>
2022-11-10 02:10:00 +0000

    Moved Frequency enum from ohlc/v1 to data

--- data/ohlc.pb.go
+++ data/ohlc.pb.go
@@ -21,6 +21,83 @@ const (
 	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
 
+// Describes the frequency of the bar
+type Frequency int32
+
+const (
+	// Describes an invalid frequency. This is used for deserializing the frequency from JSON
+	Frequency_InvalidFrequency Frequency = 0
+	// Bars should be aggregated in terms of seconds
+	Frequency_Second Frequency = 1
+	// Bars should be aggregated in terms of minutes
+	Frequency_Minute Frequency = 2
+	// Bars should be aggregated in terms of hours
+	Frequency_Hour Frequency = 3
+	// Bars should be aggregated in terms of days
+	Frequency_Day Frequency = 4
+	// Bars should be aggregated in terms of weeks
+	Frequency_Week Frequency = 5
+	// Bars should be aggregated in terms of months
+	Frequency_Month Frequency = 6
+	// Bars should be aggregated in terms of quarters
+	Frequency_Quarter Frequency = 7
+	// Bars should be aggregated in terms of years
+	Frequency_Year Frequency = 8
+)
+
+// Enum value maps for Frequency.
+var (
+	Frequency_name = map[int32]string{
+		0: "InvalidFrequency",
+		1: "Second",
+		2: "Minute",
+		3: "Hour",
+		4: "Day",
+		5: "Week",
+		6: "Month",
+		7: "Quarter",
+		8: "Year",
+	}
+	Frequency_value = map[string]int32{
+		"InvalidFrequency": 0,
+		"Second":           1,
+		"Minute":           2,
+		"Hour":             3,
+		"Day":              4,
+		"Week":             5,
+		"Month":            6,
+		"Quarter":          7,
+		"Year":             8,
+	}
+)
+
+func (x Frequency) Enum() *Frequency {
+	p := new(Frequency)
+	*p = x
+	return p
+}
+
+func (x Frequency) String() string {
+	return protoimpl.X.EnumStringOf(x.Descriptor(), protoreflect.EnumNumber(x))
+}
+
+func (Frequency) Descriptor() protoreflect.EnumDescriptor {
+	return file_protos_frontend_data_ohlc_proto_enumTypes[0].Descriptor()
+}
+
+func (Frequency) Type() protoreflect.EnumType {
+	return &file_protos_frontend_data_ohlc_proto_enumTypes[0]
+}
+
+func (x Frequency) Number() protoreflect.EnumNumber {
+	return protoreflect.EnumNumber(x)
+}
+
+// Deprecated: Use Frequency.Descriptor instead.
+func (Frequency) EnumDescriptor() ([]byte, []int) {
+	return file_protos_frontend_data_ohlc_proto_rawDescGZIP(), []int{0}
+}
+
 // Describes a single aggregated bar of data
 type Bar struct {
 	state         protoimpl.MessageState
@@ -156,10 +233,17 @@ var file_protos_frontend_data_ohlc_proto_rawDesc = []byte{
 	0x74, 0x61, 0x6d, 0x70, 0x18, 0x09, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x1c, 0x2e, 0x70, 0x72, 0x6f,
 	0x74, 0x6f, 0x73, 0x2e, 0x63, 0x6f, 0x6d, 0x6d, 0x6f, 0x6e, 0x2e, 0x55, 0x6e, 0x69, 0x78, 0x54,
 	0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x52, 0x09, 0x74, 0x69, 0x6d, 0x65, 0x73, 0x74,
-	0x61, 0x6d, 0x70, 0x42, 0x27, 0x5a, 0x25, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f,
-	0x6d, 0x2f, 0x78, 0x65, 0x66, 0x69, 0x6e, 0x6f, 0x2f, 0x71, 0x75, 0x61, 0x6e, 0x74, 0x75, 0x6d,
-	0x2d, 0x61, 0x70, 0x69, 0x2d, 0x67, 0x6f, 0x2f, 0x64, 0x61, 0x74, 0x61, 0x62, 0x06, 0x70, 0x72,
-	0x6f, 0x74, 0x6f, 0x33,
+	0x61, 0x6d, 0x70, 0x2a, 0x78, 0x0a, 0x09, 0x46, 0x72, 0x65, 0x71, 0x75, 0x65, 0x6e, 0x63, 0x79,
+	0x12, 0x14, 0x0a, 0x10, 0x49, 0x6e, 0x76, 0x61, 0x6c, 0x69, 0x64, 0x46, 0x72, 0x65, 0x71, 0x75,
+	0x65, 0x6e, 0x63, 0x79, 0x10, 0x00, 0x12, 0x0a, 0x0a, 0x06, 0x53, 0x65, 0x63, 0x6f, 0x6e, 0x64,
+	0x10, 0x01, 0x12, 0x0a, 0x0a, 0x06, 0x4d, 0x69, 0x6e, 0x75, 0x74, 0x65, 0x10, 0x02, 0x12, 0x08,
+	0x0a, 0x04, 0x48, 0x6f, 0x75, 0x72, 0x10, 0x03, 0x12, 0x07, 0x0a, 0x03, 0x44, 0x61, 0x79, 0x10,
+	0x04, 0x12, 0x08, 0x0a, 0x04, 0x57, 0x65, 0x65, 0x6b, 0x10, 0x05, 0x12, 0x09, 0x0a, 0x05, 0x4d,
+	0x6f, 0x6e, 0x74, 0x68, 0x10, 0x06, 0x12, 0x0b, 0x0a, 0x07, 0x51, 0x75, 0x61, 0x72, 0x74, 0x65,
+	0x72, 0x10, 0x07, 0x12, 0x08, 0x0a, 0x04, 0x59, 0x65, 0x61, 0x72, 0x10, 0x08, 0x42, 0x27, 0x5a,
+	0x25, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x78, 0x65, 0x66, 0x69,
+	0x6e, 0x6f, 0x2f, 0x71, 0x75, 0x61, 0x6e, 0x74, 0x75, 0x6d, 0x2d, 0x61, 0x70, 0x69, 0x2d, 0x67,
+	0x6f, 0x2f, 0x64, 0x61, 0x74, 0x61, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -174,13 +258,15 @@ func file_protos_frontend_data_ohlc_proto_rawDescGZIP() []byte {
 	return file_protos_frontend_data_ohlc_proto_rawDescData
 }
 
+var file_protos_frontend_data_ohlc_proto_enumTypes = make([]protoimpl.EnumInfo, 1)
 var file_protos_frontend_data_ohlc_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
 var file_protos_frontend_data_ohlc_proto_goTypes = []interface{}{
-	(*Bar)(nil),                // 0: protos.frontend.data.Bar
-	(*gopb.UnixTimestamp)(nil), // 1: protos.common.UnixTimestamp
+	(Frequency)(0),             // 0: protos.frontend.data.Frequency
+	(*Bar)(nil),                // 1: protos.frontend.data.Bar
+	(*gopb.UnixTimestamp)(nil), // 2: protos.common.UnixTimestamp
 }
 var file_protos_frontend_data_ohlc_proto_depIdxs = []int32{
-	1, // 0: protos.frontend.data.Bar.timestamp:type_name -> protos.common.UnixTimestamp
+	2, // 0: protos.frontend.data.Bar.timestamp:type_name -> protos.common.UnixTimestamp
 	1, // [1:1] is the sub-list for method output_type
 	1, // [1:1] is the sub-list for method input_type
 	1, // [1:1] is the sub-list for extension type_name
@@ -212,13 +298,14 @@ func file_protos_frontend_data_ohlc_proto_init() {
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_protos_frontend_data_ohlc_proto_rawDesc,
-			NumEnums:      0,
+			NumEnums:      1,
 			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
 		GoTypes:           file_protos_frontend_data_ohlc_proto_goTypes,
 		DependencyIndexes: file_protos_frontend_data_ohlc_proto_depIdxs,
+		EnumInfos:         file_protos_frontend_data_ohlc_proto_enumTypes,
 		MessageInfos:      file_protos_frontend_data_ohlc_proto_msgTypes,
 	}.Build()
 	File_protos_frontend_data_ohlc_proto = out.File
